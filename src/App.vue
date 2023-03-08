1361907aac8619646bcd07de7f5047e80b561644
Alexander Rudolph <alexr30@web.de>
2023-03-08 19:06:47 +0100

    Source Code link

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
       <Suspense>
         <router-view :key="$route.fullPath"/>
       </Suspense>
+      <v-footer app align="center">
+        <v-row justify="center">
+          <v-col align="center">
+            <a href="https://github.com/drblaui/tutorium-stationsarbeit" target="_blank">Source Code</a>
+          </v-col>
+        </v-row>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
